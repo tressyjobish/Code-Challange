a1e833273db74806e488b949337c96def8fd6b59
tressyjobish <tressyjobish@gmail.com>
2022-07-21 23:09:06 +0530

    feat:final analysis

--- DataStructure/script.js
+++ DataStructure/script.js
@@ -58,3 +58,56 @@ function koo() {
 }
 
 foo();
+
+// computing correlation
+
+function phi(table){
+  return(table[3] * table[0] - table[2] * table[1])
+  Math.sqrt((table[2] + table[3])*
+  (table[0] + table[1])*
+  (table[1] + table[3])*
+  (table[1] + table[2]))
+;
+}
+console.log(phi([76,9,4,1]));
+//0.068599434
+
+function tableFor(evet,journal) {
+  let table = [0,0,0,0];
+  for(let i=0; i<journal.length;i++){
+    let entry =journal[i],index=0;
+    if (entry.events.includes(events)) index+=1;
+    if (entry.sqirrel) index +=2;
+    table[index]+=1;
+  }
+  return table;
+}
+console.log(tableFor("pizza",JOURNAL));
+//[76,9,4,1]
+
+// ArrAY Loops
+for (let i=0; i<JOURNAL.length;i++){
+  let entry = JOURNAL[i];
+  //Do something with entry
+}
+for(let entry of JOURNAL){
+  console.log('&{entry.events.length} events');
+}
+
+// The Final Analysis
+function journalEvents(journal){
+  let events=[];
+  for (let entry of journal){
+    for (let event of entry.events){
+      if(!events.includes(event)){
+        events.push(event);
+      }
+    }
+  }
+  console.log(journalEvents(JOURNAL));
+
+  
+}
+for (let event of journalEvents(JOURNAL)){
+  console.log(events + ":", phi(tableFor(event,JOURNAL)))
+}
